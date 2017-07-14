ffba731eb0fc263579d88261777a36f3d9fed966
Damianos Christophides <chdamianos@gmail.com>
2017-07-14 11:52:19 +0100

    Added more comments

--- PCA_bootstrap.py
+++ PCA_bootstrap.py
@@ -1,116 +1,157 @@
-def PCA(arr,thre):
-    #----------------------------------------------------------------------
-    # PCA implementation
-    
-    import numpy as np    
+"""
+code author: Damianos Christophides 
+email: chdamianos@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
+"""
+
+
+def PCA(arr, thre):
+    """
+    Function to implement PCA
+    Input: array with observations as rows and variables as columns, 
+    threshold to use for explained variance, e.g. 0.95 is 95%
+    Output: Principal components (PC), Eigenvectors, Cumulative explained variance,
+    mean vector used to centre data
+    """
+
+    import numpy as np
     # Centre the data
-    mean_arr=np.mean(arr,axis=0)
-    arr_central=arr-mean_arr
+    mean_arr = np.mean(arr, axis=0)
+    arr_central = arr - mean_arr
     # Calculate covariance matrix
-    m,n=np.shape(arr_central)
-    cov_matrix=(1.0/(m-1.0))*np.dot(arr_central.T,arr_central)
+    m, n = np.shape(arr_central)
+    cov_matrix = (1.0 / (m - 1.0)) * np.dot(arr_central.T, arr_central)
     # Perform singular value decomposition to derive eigenvectors and eigenvalues
-    u,s,v=np.linalg.svd(cov_matrix)
+    u, s, v = np.linalg.svd(cov_matrix)
     # Tranform data to derive principal compoenents
-    PC=np.dot(arr_central,u)
-    EigenVectors=u
+    PC = np.dot(arr_central, u)
+    EigenVectors = u
     # Calculate cumulative explained variance from eigenvalues
-    explained_variance=s/np.sum(s)
-    explained_variance=np.cumsum(explained_variance)
-    thre_idx=np.where(explained_variance>=thre)[0][0]
-    
-    return PC[:,:thre_idx+1], EigenVectors[:,:thre_idx+1], explained_variance,mean_arr 
-    
-def conf_interval(arr,alpha):
+    explained_variance = s / np.sum(s)
+    explained_variance = np.cumsum(explained_variance)
+    thre_idx = np.where(explained_variance >= thre)[0][0]
+
+    return PC[:, :thre_idx + 1], EigenVectors[:, :thre_idx + 1], explained_variance, mean_arr
+
+
+def conf_interval(arr, alpha):
+    """
+    Function to produce confidence intervals for eigenvectors
+    Input: array with rows as eigenvectors, alpha to use in confidence interval 
+    e.g. 0.05 corresponds to 95% confidence
+    Output: mean eigenvector, lower % confidence limit, upper % confidence limit
+    """
     #----------------------------------------------------------------------
     # Import necessary modules
     import numpy as np
-    import statsmodels.api as sm 
+    import statsmodels.api as sm
 
-    no_rows, n_cols=arr.shape
-    lower_val=np.zeros(n_cols)
-    upper_val=np.zeros(n_cols)
-    mean_val=np.zeros(n_cols)
+    no_rows, n_cols = arr.shape
+    lower_val = np.zeros(n_cols)
+    upper_val = np.zeros(n_cols)
+    mean_val = np.zeros(n_cols)
 
     #----------------------------------------------------------------------
     # Calculation of confidence intervals from distribution of eigenvector values
     # and calculation of mean
-    count_=0
-    for column in arr.T:   
+    count_ = 0
+    for column in arr.T:
         # Calculate empirical cumulative distribution function for each variable
         # i.e column in the dataset
         ecdf = sm.distributions.ECDF(column)
-        x = np.linspace(np.min(column), np.max(column),num=len(column))
-        y=ecdf(x)
-        # From the ecdf calculated confidence intervals based on alpha value 
+        x = np.linspace(np.min(column), np.max(column), num=len(column))
+        y = ecdf(x)
+        # From the ecdf calculated confidence intervals based on alpha value
         # using interpolation
-        lower_val[count_]=np.interp(alpha/2., y, x)
-        upper_val[count_]=np.interp(1.0-(alpha/2.), y, x)
+        lower_val[count_] = np.interp(alpha / 2., y, x)
+        upper_val[count_] = np.interp(1.0 - (alpha / 2.), y, x)
         # Calculate mean value
-        mean_val[count_]=np.mean(column)
-        count_=count_+1
-        
-    return mean_val, lower_val, upper_val 
+        mean_val[count_] = np.mean(column)
+        count_ = count_ + 1
 
-def PCA_bootstrap(data,evs,n_boots,ci_,x,plot_cols,x_lbl,tlt):
+    return mean_val, lower_val, upper_val
+
+
+def PCA_bootstrap(data, evs, n_boots, ci_, x, plot_cols, x_lbl, tlt):
+    """
+    Function to peform eigenvector bottstrap confidence analysis
+    Input: data: dataframe with observations as rows, evs: original eigenvectors,
+    n_boots: number of bootstrap samples, ci: confidence interval 
+    (0.95 corresponds to 95%), x: vectors with x values, x_lbl: Labels of x-axis plot,
+    tlt: plot title
+    Output: plots original eigenvectors with mean and confidence intervals
+    """
     #----------------------------------------------------------------------
     # Import necessary modules
     import numpy as np
-    from sklearn.utils import resample    
-    import matplotlib.pyplot as plt   
+    from sklearn.utils import resample
+    import matplotlib.pyplot as plt
     import tqdm
- 
-    alpha_=1.0-ci_ #set alpha level based on confidence interval
-    rows,cols=evs.shape
-    test_arr=np.arange(0,len(data))    
+
+    alpha_ = 1.0 - ci_  # set alpha level based on confidence interval
+    rows, cols = evs.shape
+    test_arr = np.arange(0, len(data))
 
     #----------------------------------------------------------------------
-    # Empty array to hold the boostrap-calculated eigenvectors    
-    empty_boot=np.zeros([cols,rows,n_boots]) 
+    # Empty array to hold the boostrap-calculated eigenvectors
+    empty_boot = np.zeros([cols, rows, n_boots])
 
     #----------------------------------------------------------------------
-    # Bootstrap process    
+    # Bootstrap process
     for i in tqdm.tqdm(range(n_boots), desc='PCA Bootstrap with sample size {}'.format(len(data))):
         # Copy and take bootstrap sample of data
-        temp_=np.copy(data)
-        test_sample=resample(test_arr,n_samples=len(test_arr),replace=True)
-        arr_sample=temp_[test_sample]
+        temp_ = np.copy(data)
+        test_sample = resample(test_arr, n_samples=len(test_arr), replace=True)
+        arr_sample = temp_[test_sample]
         # Perform PCA
-        PCs_, EV_, exp_var_,mean_arr_ = PCA(np.array(arr_sample),0.99999)
+        PCs_, EV_, exp_var_, mean_arr_ = PCA(np.array(arr_sample), 0.99999)
         for j in range(cols):
-            # If necessary invert eigenvectors so they are pointing in the same 
+            # If necessary invert eigenvectors so they are pointing in the same
             # direction based on their maximum magnitude
-            sign_evs_idx=np.argmax(np.abs(evs[:,j]))      
-            if np.sign(evs[sign_evs_idx,j]) == np.sign(EV_[sign_evs_idx,j]):
-                empty_boot[j,:,i]=EV_[:,j]
+            sign_evs_idx = np.argmax(np.abs(evs[:, j]))
+            if np.sign(evs[sign_evs_idx, j]) == np.sign(EV_[sign_evs_idx, j]):
+                empty_boot[j, :, i] = EV_[:, j]
             else:
-                empty_boot[j,:,i]=-1.0*EV_[:,j]
+                empty_boot[j, :, i] = -1.0 * EV_[:, j]
 
     #----------------------------------------------------------------------
-    # Plots                   
-    plot_rows=int(np.ceil(cols/float(plot_cols)))
-	
-    fig, ax_=plt.subplots(plot_rows,plot_cols, sharex=True, sharey=False,
-							figsize=(8,6))
-    axs=ax_.ravel()
+    # Plots
+    plot_rows = int(np.ceil(cols / float(plot_cols)))
+
+    fig, ax_ = plt.subplots(plot_rows, plot_cols, sharex=True, sharey=False,
+                            figsize=(8, 6))
+    axs = ax_.ravel()
     for k in range(cols):
-        # Extract mean, upper and lower confidence intervals from 
-        # bootstrap-calculated eigenvectors      
-        mean_, low_, up_=conf_interval(empty_boot[k,:,:].T,alpha_) 
-        # Plot original eigenvector, mean and confidence intervals     
-        axs[k].plot(x,evs[:,k],'k',label='Original Eigenvector')
-        axs[k].plot(x,mean_,'--k',label='Mean Eigenvector')
-        axs[k].fill_between(x,low_,up_,alpha=0.5, edgecolor='#000000', facecolor='#000000')
-        axs[k].set_title('Eigenvector '+str(k+1)) 
-		
-        if k % plot_cols == 0:
-            axs[k].set_ylabel('Weight',fontsize=12, fontweight='bold')
-        if k >= ((plot_rows-1)*plot_cols):
-            axs[k].set_xlabel(x_lbl,fontsize=12, fontweight='bold')   
+        # Extract mean, upper and lower confidence intervals from
+        # bootstrap-calculated eigenvectors
+        mean_, low_, up_ = conf_interval(empty_boot[k, :, :].T, alpha_)
+        # Plot original eigenvector, mean and confidence intervals
+        axs[k].plot(x, evs[:, k], 'k', label='Original Eigenvector')
+        axs[k].plot(x, mean_, '--k', label='Mean Eigenvector')
+        axs[k].fill_between(x, low_, up_, alpha=0.5,
+                            edgecolor='#000000', facecolor='#000000')
+        axs[k].set_title('Eigenvector ' + str(k + 1))
 
-    axs[0].legend()          
+        if k % plot_cols == 0:
+            axs[k].set_ylabel('Weight', fontsize=12, fontweight='bold')
+        if k >= ((plot_rows - 1) * plot_cols):
+            axs[k].set_xlabel(x_lbl, fontsize=12, fontweight='bold')
+
+    axs[0].legend()
     fig.suptitle(tlt, fontsize="x-large")
-    
+
     #----------------------------------------------------------------------
-    # Return bootstrap-calculated eigenvectors       
-    return empty_boot       
+    # Return bootstrap-calculated eigenvectors
+    return empty_boot
--- example.py
+++ example.py
@@ -1,20 +1,34 @@
 #----------------------------------------------------------------------
-# Code source: Damianos Christophides
-# email: chdamianos@gmail.com
+"""
+code author: Damianos Christophides 
+email: chdamianos@gmail.com
 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
+"""
 #----------------------------------------------------------------------
 # Import necessary modules
 import PCA_bootstrap
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt   
-from sklearn.cluster import KMeans   
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 #----------------------------------------------------------------------
 # Import and plot data
 # Data downloaded from http://openmv.net/file/tablet-spectra.csv
-data=pd.read_csv('tablet-spectra.csv',index_col=0,header=None)
-data.T.plot(use_index=True,legend=False)
+data = pd.read_csv('tablet-spectra.csv', index_col=0, header=None)
+data.T.plot(use_index=True, legend=False)
 plt.xlabel('Wavelength [nm]')
 plt.ylabel('Absorbance')
 plt.title('Spectral Data')
@@ -23,44 +37,45 @@ plt.title('Spectral Data')
 # Perform PCA
 # Return principal components (pcs), eigenvectors (evs), explained_variance
 # and mean_vector used to centre the data
-pcs, evs, explained_variance,mean_vector=PCA_bootstrap.PCA(np.array(data),0.95)
+pcs, evs, explained_variance, mean_vector = PCA_bootstrap.PCA(
+    np.array(data), 0.95)
 
 #----------------------------------------------------------------------
 # Perform bootstrap analysis to extract the mean eigenvector
-# and the 95% confidence interval of the eigenvector distribution 
-x_axis=range(data.shape[1])
-x_label='Spectral Channel [AU]'
-n_boot_samples=100
-confidence_interval=0.95
-plot_columns=2
+# and the 95% confidence interval of the eigenvector distribution
+x_axis = range(data.shape[1])
+x_label = 'Spectral Channel [AU]'
+n_boot_samples = 100
+confidence_interval = 0.95
+plot_columns = 2
 # define sample sizes to run the bootstrap over
-sample_size=np.array([50,100,300,450])
+sample_size = np.array([50, 100, 300, 450])
 for sz in sample_size:
-    data_temp=data.copy()
+    data_temp = data.copy()
     data_temp = data_temp.sample(n=sz)
-    fig_title='Number of observations: {}'.format(sz)
-    boot_output=PCA_bootstrap.PCA_bootstrap(data_temp,evs,n_boot_samples,confidence_interval,
-                                         x_axis,plot_columns,x_label,fig_title)
-    
-#----------------------------------------------------------------------    
+    fig_title = 'Number of observations: {}'.format(sz)
+    boot_output = PCA_bootstrap.PCA_bootstrap(data_temp, evs, n_boot_samples, confidence_interval,
+                                              x_axis, plot_columns, x_label, fig_title)
+
+#----------------------------------------------------------------------
 # Out of interest, plot first 2 prinipal components from spectral sample
 plt.figure()
-ax=plt.subplot(111)
-ax.scatter(pcs[:,0],pcs[:,1])
+ax = plt.subplot(111)
+ax.scatter(pcs[:, 0], pcs[:, 1])
 ax.set_xlabel('PC1')
 ax.set_ylabel('PC2')
 ax.set_title('First two principal components')
 # There seems to be 2 clusters of data
 
-#----------------------------------------------------------------------    
+#----------------------------------------------------------------------
 # Apply K-means clustering and plot results
-est_clusters=KMeans(n_clusters=2)
-est_clusters.fit(pcs[:,:2])
+est_clusters = KMeans(n_clusters=2)
+est_clusters.fit(pcs[:, :2])
 labels = est_clusters.labels_
 
 plt.figure()
-ax=plt.subplot(111)
-ax.scatter(pcs[:,0],pcs[:,1],c=labels.astype(np.float))
+ax = plt.subplot(111)
+ax.scatter(pcs[:, 0], pcs[:, 1], c=labels.astype(np.float))
 ax.set_xlabel('PC1')
 ax.set_ylabel('PC2')
 ax.set_title('K-Means clusters of first two principal components')
